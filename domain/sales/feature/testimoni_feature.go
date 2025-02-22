da495a7dfed2d7547457d2e412f8e83cebc2589d
zanuarrahmanu <zanusecondmail@gmail.com>
2025-02-22 22:25:07 +0700

    FIX : testimony

---
+++ domain/sales/feature/testimoni_feature.go
@@ -0,0 +1,63 @@
+package feature
+
+import (
+	"context"
+	paginate "zayyid-go/domain/shared/helper/pagination"
+	sharedModel "zayyid-go/domain/shared/model"
+	sharedRepo "zayyid-go/domain/shared/repository"
+
+	modelRequest "zayyid-go/domain/sales/model/request"
+)
+
+func (f SalesFeature) AddTestimoniFeature(ctx context.Context, request modelRequest.Testimoni) (err error) {
+
+	request.Id = sharedRepo.GenerateUuidAsIdTable().String()
+	err = f.repo.AddTestimoniRepository(ctx, request)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (f SalesFeature) UpdateTestimoniFeature(ctx context.Context, request modelRequest.Testimoni) (err error) {
+
+	err = f.repo.UpdateTestimoniRepository(ctx, request)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (f SalesFeature) GetTestimoniFeature(ctx context.Context, request modelRequest.Testimoni) (response modelRequest.Testimoni, err error) {
+
+	response, err = f.repo.GetTestimoniRepository(ctx, request)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (f SalesFeature) GetListTestimoniFeature(ctx context.Context, request modelRequest.Testimoni, filter modelRequest.TestimoniSearch) (response []modelRequest.Testimoni, pagination *sharedModel.Pagination, err error) {
+
+	response, err = f.repo.GetListTestimoniRepository(ctx, request, filter)
+	if err != nil {
+		return
+	}
+
+	count, err := f.repo.CountListTestimoniRepository(ctx, request)
+	if err != nil {
+		return
+	}
+
+	pagination, err = paginate.CalculatePagination(ctx, filter.Limit, count)
+	if err != nil {
+		return
+	}
+
+	pagination.Page = filter.Page
+
+	return
+}
---
+++ domain/sales/repository/testimoni_repository.go
@@ -0,0 +1,221 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	modelRequest "zayyid-go/domain/sales/model/request"
+	"zayyid-go/domain/shared/helper/constant"
+	"zayyid-go/infrastructure/logger"
+)
+
+func (t salesRepository) AddTestimoniRepository(ctx context.Context, request modelRequest.Testimoni) (err error) {
+
+	args := []interface{}{
+		request.Id,
+		request.UserName,
+		request.Position,
+		request.Deskripsi,
+		request.PhotoUrl,
+		request.IsActive,
+	}
+
+	query := `
+		INSERT INTO testimoni (id, user_name, position, deskripsi, photo_url, is_active, created_at)
+		VALUES
+			($1,$2,$3,$4,$5,$6,NOW())`
+
+	stmt, err := t.database.Preparex(query)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-prepare-create] | " + err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, args...)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-create] | " + err.Error())
+		return
+	}
+
+	return
+}
+
+func (t salesRepository) UpdateTestimoniRepository(ctx context.Context, request modelRequest.Testimoni) (err error) {
+
+	args := []interface{}{}
+	buildQuery := []string{}
+
+	if request.Position != "" {
+		args = append(args, request.Position)
+		buildQuery = append(buildQuery, " position = $1")
+	}
+	if request.Deskripsi != "" {
+		args = append(args, request.Deskripsi)
+		buildQuery = append(buildQuery, " deskripsi = $2")
+	}
+	if request.PhotoUrl != "" {
+		args = append(args, request.PhotoUrl)
+		buildQuery = append(buildQuery, " photo_url = $3")
+	}
+
+	args = append(args, request.IsActive)
+	buildQuery = append(buildQuery, " is_active = $4")
+	buildQuery = append(buildQuery, " modified_at = NOW()")
+
+	updateQuery := strings.Join(buildQuery, ",")
+	args = append(args, request.Id)
+	args = append(args, request.UserName)
+	query := fmt.Sprintf(`UPDATE testimoni SET %s  WHERE id = ? AND user_name = ? `, updateQuery)
+
+	stmt, err := t.database.Preparex(query)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-prepare-update] | " + err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, args...)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-update] | " + err.Error())
+		return
+	}
+
+	return
+}
+
+func (t salesRepository) GetTestimoniRepository(ctx context.Context, request modelRequest.Testimoni) (response modelRequest.Testimoni, err error) {
+
+	query := `
+		SELECT
+			id, 
+			user_name, 
+			position, 
+			deskripsi, 
+			photo_url, 
+			is_active, 
+			created_at,
+			modified_at
+		FROM
+			testimoni
+		WHERE
+			id = $1`
+	logger.LogInfo(constant.QUERY, query)
+
+	stmt, err := t.database.Preparex(query)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-prepare-get] | " + err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.GetContext(ctx, response)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-get] | " + err.Error())
+		return
+	}
+
+	return
+}
+
+func (t salesRepository) GetListTestimoniRepository(ctx context.Context, request modelRequest.Testimoni, filter modelRequest.TestimoniSearch) (response []modelRequest.Testimoni, err error) {
+
+	var (
+		args     []interface{}
+		argIndex = 1
+	)
+
+	offset := (filter.Page - 1) * filter.Limit
+
+	queryCond := ""
+	if request.UserName != "" {
+		args = append(args, request.UserName)
+		queryCond += fmt.Sprintf(" AND user_name = $%d", argIndex)
+		argIndex++
+	}
+
+	querySort := ""
+	if filter.SortBy != "" {
+		querySort += " ORDER BY " + filter.SortBy
+
+		if filter.SortOrder != "" {
+			querySort += " " + filter.SortOrder
+		}
+	}
+
+	args = append(args, filter.Limit, offset)
+	queryLimit := fmt.Sprintf(" LIMIT $%d OFFSET $%d", argIndex, argIndex+1)
+
+	query := fmt.Sprintf(`
+		SELECT
+			id, 
+			user_name, 
+			position, 
+			deskripsi, 
+			photo_url, 
+			is_active, 
+			created_at,
+			modified_at
+		FROM
+			testimoni
+		WHERE
+			1 = 1
+			%s %s`, queryCond, queryLimit)
+	logger.LogInfo(constant.QUERY, query)
+
+	stmt, err := t.database.Preparex(query)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-prepare-get] | " + err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.SelectContext(ctx, response, args...)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-get] | " + err.Error())
+		return
+	}
+
+	return
+}
+
+func (t salesRepository) CountListTestimoniRepository(ctx context.Context, request modelRequest.Testimoni) (response int, err error) {
+
+	var (
+		args     []interface{}
+		argIndex = 1
+	)
+
+	queryCond := ""
+	if request.UserName != "" {
+		args = append(args, request.UserName)
+		queryCond += fmt.Sprintf(" AND user_name = $%d", argIndex)
+		argIndex++
+	}
+
+	query := fmt.Sprintf(`
+		SELECT
+			COUNT(id)
+		FROM
+			testimoni
+		WHERE
+			1 = 1
+			%s`, queryCond)
+	logger.LogInfo(constant.QUERY, query)
+
+	stmt, err := t.database.Preparex(query)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-prepare-count] | " + err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.GetContext(ctx, response, args...)
+	if err != nil {
+		err = errors.New("🔥 [testimoni-count] | " + err.Error())
+		return
+	}
+
+	return
+}
