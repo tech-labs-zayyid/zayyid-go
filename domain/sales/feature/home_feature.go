df43d82717d07ccff813e6c86d14b88bb9b04687
jefri_widianto <jefri.widianto.ext@danamas.co.id>
2025-03-01 14:22:35 +0700

    dev, change log: - mocking data home

--- domain/sales/feature/home_feature.go
+++ domain/sales/feature/home_feature.go
@@ -23,12 +23,36 @@ func (f SalesFeature) HomeSalesData(ctx context.Context, subdomain, referral str
 	if referral != "" {
 		// logic validation referral
 
-		//mocking value
-		resp.Agent.PhoneNumber = "0897567474747"
-		resp.Agent.Fullname = "dhany"
-		resp.Agent.Email = "www@gmail.com"
 	}
 
 	resp, err = f.repo.HomeData(ctx, subdomain)
+
+	//mocking value
+	resp.Agent.PhoneNumber = "0897567474747"
+	resp.Agent.Fullname = "dhany"
+	resp.Agent.Email = "www@gmail.com"
+
+	resp.Fullname = "eko eka eke"
+	resp.PhoneNumber = "087635342667"
+	resp.Email = "eko@mail.com"
+	resp.Desc = "Stay Humble, peace love & Gaul"
+
+	resp.Testimony = append(resp.Testimony, response.TestimonyListHome{
+		IdTestimony: "01955073-a98d-7707-954b-540d8c37034b",
+		Name:        "Jefri Santuy",
+		Description: "waah keyen",
+		PhotoUrl:    "https://res.cloudinary.com/dyj8vcauy/image/upload/v1740811698/WhatsApp_Image_2025-02-28_at_17.46.01_ukk40e.jpg",
+	})
+
+	resp.Product = append(resp.Product, response.BestProduct{
+		IdProduct:          "0195508a-8ec1-7a78-8236-8ab98a4854a7",
+		ProductName:        "Lambo Lambe",
+		Price:              1.500000000,
+		ProductSubCategory: "Sports Car",
+		TDP:                700000000,
+		Installment:        125000000,
+		CityId:             "01951519-c232-775a-b8ef-76c2d0e2fa7e",
+		BestProduct:        true,
+	})
 	return
 }
